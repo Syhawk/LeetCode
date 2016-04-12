c5bd2848b481ea705c3454a9b81c7451a27b8d31
Syhawk <805782175@qq.com>
2016-04-12 09:51:19 +0800

    2016年04月12日09:51:18

---
+++ majority_element_ii.cpp
@@ -0,0 +1,40 @@
+//uri:	https://leetcode.com/problems/majority-element-ii/
+
+/*
+ *	由于题目要求是O(1)空间复杂度，线性时间，所以，可以采用三个下标，分别表示三个不同元素的下标，找到之后，就与i, i + 1, i + 2所表示的元素
+ *	进行交换，然后第一个下标跳过i + 2，从i + 3处继续寻找三个不同的元素，直到找不到为止。然后判断i， j下标所表示的元素是否符合条件即可。
+ *	空间复杂度：O(1).
+ *	时间复杂度：O(n).
+ * */
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> v;
+        int len = nums.size();
+        for(int i = 0, j, k; i < len; i += 3) {
+            for(j = i + 1; j < len && nums[j] == nums[i]; ++ j);
+            for(k = j + 1; k < len && (nums[k] == nums[i] || nums[k] == nums[j]); ++ k);
+            
+            if(k < len) {
+                swap(nums[i + 1], nums[j]);
+                swap(nums[i + 2], nums[k]);
+                continue;
+            }
+            if(j < len && majority(nums[j], nums, len)) { v.push_back(nums[j]); }
+            if(majority(nums[i], nums, len)) { v.push_back(nums[i]); }
+            i = len;
+        }
+        
+        return v;
+    }
+    
+    bool majority(int val, vector<int>& nums, int len) {
+        int cnt = 0;
+        for(int i = 0; i < len; ++ i) {
+            if(nums[i] == val) { cnt += 1; }
+        }
+        
+        return (cnt > len / 3);
+    }
+};
