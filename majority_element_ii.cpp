11dd575d8557f16baedce85adc4e78361b11c914
Syhawk <805782175@qq.com>
2016-04-14 20:16:54 +0800

    2016年04月14日20:16:53

--- majority_element_ii.cpp
+++ majority_element_ii.cpp
@@ -38,3 +38,60 @@ public:
         return (cnt > len / 3);
     }
 };
+
+/*
+ *	用两个变量分别表示两个不同的元素，再用另外两个变量表示这两个元素出现的次数，遇到的元素值如果是这两个元素中的一个，那么该元素次数增加1，
+ *	否则这两个元素均减1。如果两个变量有空缺，即出现的次数为0，那么就用新的元素替代。
+ *	空间复杂度：O(1).
+ *	时间复杂度：O(n).
+ * */
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        const int N = 2;
+        int len = nums.size();
+        vector<int> a(N, 0);
+        vector<int> cnt(N, 0);
+
+        for (int i = 0; i < len; ++ i) {
+            int w = -1;
+            for (int k = 0; k < N; ++ k) {
+                if (a[k] == nums[i]) {
+                    w = k;
+                    break;
+                }
+            }
+            
+            if (w != -1) {
+                ++ cnt[w];
+                continue;
+            }
+                
+            for (int k = 0; k < N; ++ k) {
+                if (cnt[k] == 0){
+                    w = k;
+                    cnt[k] = 1;
+                    a[k] = nums[i];
+                    break;
+                }
+            }
+            
+            if(w != -1) { continue; }
+            for (int k = 0; k < N; ++ k) { cnt[k] -= 1; }
+        }
+          
+        vector<int> v;
+        for (int i = 0; i < N; ++ i) {
+          if(cnt[i] < 1) { continue; }
+          int sum = 0;
+          for (int j = 0; j < len; ++ j) {
+              if (nums[j] == a[i]) { sum += 1; }
+          }
+          
+          if (sum > len / 3) { v.push_back(a[i]); }
+        }
+        
+        return v;
+    }
+};
